940ed725bac7792d93fabce5969a6a50430991ce
Frn14KV <villa.chispo@gmail.com>
2025-02-26 15:37:54 -0500

    revision_5

--- kickandplay/settings.py
+++ kickandplay/settings.py
@@ -93,13 +93,11 @@ WSGI_APPLICATION = 'kickandplay.wsgi.application'
 #    }
 #}
 
-#DATABASES = {
-#    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-#}
-
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://postgres:Frn14K9542@db.fluofgltdazuwfgpnctl.supabase.co:5432/postgres')
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
+
+
 
 
 #DATABASES = {

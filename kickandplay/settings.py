13e88f06638bf1908618487a9bf8736b2b5e191e
Frn14KV <villa.chispo@gmail.com>
2025-02-26 15:28:21 -0500

    base postgresql_1

--- kickandplay/settings.py
+++ kickandplay/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'kickandplay.wsgi.application'
 #}
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://postgres:[Frn14K9542]@db.fluofgltdazuwfgpnctl.supabase.co:5432/postgres')
+    'default': dj_database_url.config(default='postgresql://postgres:Frn14K9542@db.fluofgltdazuwfgpnctl.supabase.co:5432/postgres')
 }
 
 

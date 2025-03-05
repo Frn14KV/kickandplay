47e2fa4d9994190ddbe32103c41f063ea6f88c59
Frn14KV <villa.chispo@gmail.com>
2025-03-05 18:34:09 -0500

    Agregar página principal1

--- kickandplay/urls.py
+++ kickandplay/urls.py
@@ -20,6 +20,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from canchas import views
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [

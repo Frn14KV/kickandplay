efad938184c20905c9a71a6035074edbbc7ecb0e
Frn14KV <villa.chispo@gmail.com>
2025-03-05 18:14:57 -0500

    Actualizar proyecto con nuevas funcionalidades

--- kickandplay/settings.py
+++ kickandplay/settings.py
@@ -149,8 +149,24 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # restframework
-REST_FRAMEWORK = {'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',), 'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',)}
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
+'''
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': 
+    ('rest_framework_simplejwt.authentication.JWTAuthentication',), 
+    'DEFAULT_PERMISSION_CLASSES': 
+    ('rest_framework.permissions.IsAuthenticated',)}
+'''
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/

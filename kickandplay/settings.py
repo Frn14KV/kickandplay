8242f30cda408fb60fcf755843f6ddb85c89db85
Frn14KV <villa.chispo@gmail.com>
2025-03-23 16:13:01 -0400

    actualizacion n30

--- kickandplay/settings.py
+++ kickandplay/settings.py
@@ -217,7 +217,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://localhost:8080']
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  # Flutter Web local
+    'http://localhost:8080',  # Otro entorno local
+    'https://kickandplay-3b16b2f1fd11.herokuapp.com'  # URL de tu servidor en producción
+]
+
 '''
 LOGGING = {
     'version': 1,
